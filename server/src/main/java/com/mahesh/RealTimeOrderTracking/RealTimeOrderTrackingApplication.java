f449f3db2c1c033fad96d675269382b8e9f5e095
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-05-16 11:33:08 +0530

    initial backend project commit

---
+++ server/src/main/java/com/mahesh/RealTimeOrderTracking/RealTimeOrderTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.RealTimeOrderTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeOrderTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeOrderTrackingApplication.class, args);
+	}
+
+}
