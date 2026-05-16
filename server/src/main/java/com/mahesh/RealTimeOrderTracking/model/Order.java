a4f52c09a4dbda44a05e833bb2ef15000e455a14
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-05-16 12:08:55 +0530

    initialize Order entity, repository

---
+++ server/src/main/java/com/mahesh/RealTimeOrderTracking/model/Order.java
@@ -0,0 +1,21 @@
+package com.mahesh.RealTimeOrderTracking.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String product;
+    private Integer quantity;
+    private String status;
+}
